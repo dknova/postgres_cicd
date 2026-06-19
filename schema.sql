5c78a2a20801a65a5510b90e64aed9c55fa726d1
DKNOVA <ops@dknova.in>
2026-06-19 12:39:41 +0530

    added email

--- schema.sql
+++ schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS company_clients (
     client_id SERIAL PRIMARY KEY,
     company_name VARCHAR(150) NOT NULL,
     contact_person VARCHAR(100),
-    project_budget NUMERIC(12,2)
+    project_budget NUMERIC(12,2),
+    email VARCHAR(100)
 );
